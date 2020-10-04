ce17414b57de984f4e8dfcae2560f1c1f7fea295
lilman109 <akira.yoshidasjsu@gmail.com>
2020-10-04 13:18:34 +0900

    react-router-dom trial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
+
+const HatsPage = () => (
+	<div>
+		<h1> HATS PAGE</h1>
+	</div>
+);
 
 function App() {
 	return (
 		<div>
-			<HomePage />
+			<Switch>
+				<Route exact path="/" component={HomePage}></Route>
+				<Route exact path="/shop/hats" component={HatsPage}></Route>
+			</Switch>
 		</div>
 	);
 }
