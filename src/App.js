c5c7408914c6982d0b82edfd2eb80f9cbf444369
lilman109 <akira.yoshidasjsu@gmail.com>
2020-10-11 21:20:37 +0900

    added signin signout button for header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import Header from './components/header/header.component.jsx';
+import { auth } from './firebase/firebase.utils.js';
 
-function App() {
-	return (
-		<div>
-			<Header></Header>
-			<Switch>
-				<Route exact path="/" component={HomePage}></Route>
-				<Route exact path="/shop" component={ShopPage}></Route>
-				<Route exact path="/sign-in" component={SignInSignUpPage}></Route>
-			</Switch>
-		</div>
-	);
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			currentUser: null,
+		};
+	}
+
+	unsubscribeFromAuth = null;
+
+	componentDidMount() {
+		this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+			this.setState({ currentUser: user });
+		});
+
+		console.log('component did mount');
+	}
+
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+		console.log('component will unmount');
+	}
+
+	render() {
+		return (
+			<div>
+				<Header currentUser={this.state.currentUser}></Header>
+				<Switch>
+					<Route exact path="/" component={HomePage}></Route>
+					<Route exact path="/shop" component={ShopPage}></Route>
+					<Route exact path="/sign-in" component={SignInSignUpPage}></Route>
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { auth } from '../../firebase/firebase.utils.js';
 
-const Header = () => {
+const Header = ({ currentUser }) => {
 	return (
 		<div className="header">
 			<Link className="logo-container" to="/">
@@ -16,6 +17,15 @@ const Header = () => {
 				<Link className="option" to="/shop">
 					CONTACT
 				</Link>
+				{currentUser ? (
+					<Link className="option" onClick={() => auth.signOut()}>
+						SIGN OUT
+					</Link>
+				) : (
+					<Link className="option" to="/sign-in">
+						SIGN IN
+					</Link>
+				)}
 			</div>
 		</div>
 	);
