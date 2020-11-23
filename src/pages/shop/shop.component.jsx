e87d3740931f90a86892cea0d2a9e3ff46f94478
lilman109 <akira.yoshidasjsu@gmail.com>
2020-11-23 13:56:17 +0900

    implement code that pulls in shop data from firestore and store it to shop reducer, and included loading spinner

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CollectionOverview from '../../components/collection-overview/collection-overview.component.jsx';
 import { Route } from 'react-router-dom';
 import CollectionPage from '../collection/collection.component.jsx';
@@ -8,8 +8,17 @@ import {
 } from '../../firebase/firebase.utils.js';
 import { connect } from 'react-redux';
 import { updateCollections } from '../../redux/shop/shop.actions.js';
+import WithSpinner from '../../components/with-loading/with-spinner.component.jsx';
 
-class ShopPage extends React.Component {
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+class ShopPage extends Component {
+	state = {
+		isLoading: true,
+		hoge: true,
+	};
+
 	unsubscribeFromSnapShot = null;
 
 	componentDidMount() {
@@ -19,21 +28,28 @@ class ShopPage extends React.Component {
 		this.unsubscribeFromSnapShot = collectionRef.onSnapshot((snapShot) => {
 			const collectionMap = convertCollectionsSnapshotToMap(snapShot);
 			updateCollections(collectionMap);
+			this.setState({ isLoading: false });
 		});
 	}
 
 	render() {
 		const { match } = this.props;
+		const { isLoading } = this.state;
+
 		return (
 			<div className='shop-page'>
 				<Route
 					exact
 					path={`${match.path}`}
-					component={CollectionOverview}
+					render={(props) => (
+						<CollectionOverviewWithSpinner isLoading={isLoading} {...props} />
+					)}
 				></Route>
 				<Route
 					path={`${match.path}/:categoryId`}
-					component={CollectionPage}
+					render={(props) => (
+						<CollectionPageWithSpinner isLoading={isLoading} {...props} />
+					)}
 				></Route>
 			</div>
 		);
